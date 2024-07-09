c08998999f4eff190c4858e2ba3073bd01bb61cb
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2024-07-09 23:22:27 +0900

    うごくかわからん

--- scripts/gen.py
+++ scripts/gen.py
@@ -4,15 +4,23 @@ import csv
 
 dirname = os.path.dirname(__file__)
 
-with open(
-    os.path.join(dirname, "../data/Genshin Impact/character.csv"), encoding="utf-8"
-) as f:
-    result = [";; okuri-ari entries.", ";; okuri-nasi entries."]
-    reader = csv.reader(f)
-    for row in reader:
-        result.append(f"{row[1]} /{row[0]}/")
+game_list = os.listdir(os.path.join(dirname, "../data"))
+
+for game in game_list:
+    csv_list = os.listdir(os.path.join(dirname, "../data", game))
+
+    data = []
+
+    for filename in csv_list:
+        with open(
+            os.path.join(dirname, "../data", game, filename), encoding="utf-8"
+        ) as f:
+            data.extend(csv.reader(f))
 
     with open(
-        os.path.join(dirname, "../out/genshin_impact_skk.txt"), "w", encoding="utf-8"
+        os.path.join(dirname, "../out", f"{game} SKK.txt"), "w", encoding="utf-8"
     ) as f:
+        result = [";; okuri-ari entries.", ";; okuri-nasi entries."]
+        for item in data:
+            result.append(f"{item[1]} /{item[0]}/")
         f.write("\n".join(result))
