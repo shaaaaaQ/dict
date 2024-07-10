d44a7f705b97139cbdd86d13979b8fda7e9fb977
sh <64454953+shaaaaaQ@users.noreply.github.com>
2024-07-10 23:26:50 +0900

    ひらがな

--- scripts/genshin-dictionary.py
+++ scripts/genshin-dictionary.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import csv
+import jaconv
 
 dirname = os.path.dirname(__file__)
 data_dir = os.path.join(dirname, "../data/Genshin Impact")
@@ -14,13 +15,17 @@ with open(os.path.join(data_dir, "weapon.csv"), "r", encoding="utf-8") as f:
     for data in dataset:
         if "tags" in data and "weapon" in data["tags"]:
             word = data["ja"]
-            kana = data["pronunciationJa"] if "pronunciationJa" in data else ""
+            hiragana = (
+                jaconv.kata2hira(data["pronunciationJa"])
+                if "pronunciationJa" in data
+                else ""
+            )
             hinshi = "名詞"
             comment = "原神/武器"
 
             if not list(filter(lambda row: row[0] == word, csv_data)):
                 print(word, " nai")
-                csv_data.append([word, kana, hinshi, comment])
+                csv_data.append([word, hiragana, hinshi, comment])
     with open(os.path.join(data_dir, "weapon.csv"), "w", encoding="utf-8") as f:
         writer = csv.writer(f)
         writer.writerows(csv_data)
