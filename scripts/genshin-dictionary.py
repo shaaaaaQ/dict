1584dc14d3a0020184e74ea72319c687225388b5
sh <64454953+shaaaaaQ@users.noreply.github.com>
2024-07-11 23:31:40 +0900

    genshin/artifact

--- scripts/genshin-dictionary.py
+++ scripts/genshin-dictionary.py
@@ -42,6 +42,8 @@ with open(os.path.join(data_dir, "excludes.txt"), encoding="utf-8") as f:
 weapon = {"sword": [], "claymore": [], "polearm": [], "bow": [], "catalyst": []}
 weapon_other = []
 element = []
+artifact_set = []
+artifact_piece = []
 
 for data in dataset:
     if "ja" not in data:
@@ -59,6 +61,10 @@ for data in dataset:
                 weapon_other.append(data)
         elif "element" in tags:
             element.append(data)
+        elif "artifact" in tags:
+            artifact_set.append(data)
+        elif "artifact-piece" in tags:
+            artifact_piece.append(data)
 
 write(weapon["sword"], "weapon/sword.csv", "名詞", "原神/武器/片手剣")
 write(weapon["claymore"], "weapon/claymore.csv", "名詞", "原神/武器/両手剣")
@@ -67,3 +73,5 @@ write(weapon["bow"], "weapon/bow.csv", "名詞", "原神/武器/弓")
 write(weapon["catalyst"], "weapon/catalyst.csv", "名詞", "原神/武器/法器")
 write(weapon_other, "weapon/other.csv", "名詞")
 write(element, "element.csv", "名詞")
+write(artifact_set, "artifact/set.csv", "名詞", "原神/聖遺物")
+write(artifact_piece, "artifact/piece.csv", "名詞")
