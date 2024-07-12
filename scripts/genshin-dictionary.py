e38e36dd5a9afccc692147dde345e783b3ad87c7
shaaaaaQ <64454953+shaaaaaQ@users.noreply.github.com>
2024-07-13 00:41:24 +0900

    ugokuka wakaran

--- scripts/genshin-dictionary.py
+++ scripts/genshin-dictionary.py
@@ -8,27 +8,6 @@ dirname = os.path.dirname(__file__)
 data_dir = os.path.join(dirname, "../data/Genshin Impact")
 dataset = requests.get("https://dataset.genshin-dictionary.com/words.json").json()
 
-
-def write(data_list, filepath, hinshi, comment=""):
-    with open(os.path.join(data_dir, filepath), "a", encoding="utf-8") as f:
-        print(f"新規追加 ({filepath}): ", len(data_list))
-        writer = csv.writer(f)
-        for data in data_list:
-            word = data["ja"]
-            hiragana = (
-                jaconv.kata2hira(data["pronunciationJa"])
-                if "pronunciationJa" in data
-                else ""
-            )
-            _comment = ""
-            if comment:
-                _comment = comment
-            elif "notes" in data:
-                _comment = data["notes"]
-
-            writer.writerow([word, hiragana, hinshi, _comment])
-
-
 excludes = []
 files = glob.glob(os.path.join(data_dir, "**/*.csv"), recursive=True)
 for csv_path in files:
@@ -39,39 +18,76 @@ for csv_path in files:
 with open(os.path.join(data_dir, "excludes.txt"), encoding="utf-8") as f:
     excludes.extend(f.read().splitlines())
 
-weapon = {"sword": [], "claymore": [], "polearm": [], "bow": [], "catalyst": []}
-weapon_other = []
-facility = {
-    "mondstadt": [],
-    "dragonspine": [],
-    "liyue": [],
-    "inazuma": [],
-    "sumeru": [],
-    "fontaine": [],
-    "natlan": [],
-    "snezhnaya": [],
-    "khaenriah": []
+
+q = {
+    "weapon": {
+        "sword": ["weapon/sword.csv", "名詞", "原神/武器/片手剣"],
+        "claymore": ["weapon/claymore.csv", "名詞", "原神/武器/両手剣"],
+        "polearm": ["weapon/polearm.csv", "名詞", "原神/武器/長柄武器"],
+        "bow": ["weapon/bow.csv", "名詞", "原神/武器/弓"],
+        "catalyst": ["weapon/catalyst.csv", "名詞", "原神/武器/法器"],
+        "weapon": ["weapon/other.csv", "名詞"],
+    },
+    "facility": {
+        "mondstadt": ["facility/mondstadt.csv", "名詞", "原神/施設/モンド"],
+        "dragonspine": [
+            "facility/dragonspine.csv",
+            "名詞",
+            "原神/施設/ドラゴンスパイン",
+        ],
+        "liyue": ["facility/liyue.csv", "名詞", "原神/施設/璃月"],
+        "inazuma": ["facility/inazuma.csv", "名詞", "原神/施設/稲妻"],
+        "sumeru": ["facility/sumeru.csv", "名詞", "原神/施設/スメール"],
+        "fontaine": ["facility/fontaine.csv", "名詞", "原神/施設/フォンテーヌ"],
+        "natlan": ["facility/natlan.csv", "名詞", "原神/施設/ナタ"],
+        "snezhnaya": ["facility/snezhnaya.csv", "名詞", "原神/施設/スネージナヤ"],
+        "khaenriah": ["facility/khaenriah.csv", "名詞", "原神/施設/カーンルイア"],
+        "facility": ["facility/other.csv", "名詞"],
+    },
+    "location": {
+        "mondstadt": ["area/mondstadt.csv", "地名その他", "原神/地名/モンド"],
+        "dragonspine": [
+            "area/dragonspine.csv",
+            "地名その他",
+            "原神/地名/ドラゴンスパイン",
+        ],
+        "liyue": ["area/liyue.csv", "地名その他", "原神/地名/璃月"],
+        "inazuma": ["area/inazuma.csv", "地名その他", "原神/地名/稲妻"],
+        "sumeru": ["area/sumeru.csv", "地名その他", "原神/地名/スメール"],
+        "fontaine": ["area/fontaine.csv", "地名その他", "原神/地名/フォンテーヌ"],
+        "natlan": ["area/natlan.csv", "地名その他", "原神/地名/ナタ"],
+        "snezhnaya": ["area/snezhnaya.csv", "地名その他", "原神/地名/スネージナヤ"],
+        "khaenriah": ["area/khaenriah.csv", "地名その他", "原神/地名/カーンルイア"],
+        "location": ["area/other.csv", "地名その他"],
+    },
+    "drop": ["item/drop.csv", "名詞", "原神/アイテム/ドロップ"],
+    "drop-boss": ["item/drop-boss.csv", "名詞", "原神/アイテム/ボスドロップ"],
+    "talent-material": ["item/talent-material.csv", "名詞", "原神/アイテム/天賦素材"],
+    "weapon-material": ["item/weapon-material.csv", "名詞", "原神/アイテム/武器素材"],
+    "element": ["element.csv", "名詞"],
+    "artifact-set": ["artifact/set.csv", "名詞", "原神/聖遺物"],
+    "artifact-piece": ["artifact/piece.csv", "名詞"],
+    "character-main": ["character/playable.csv", "人名", "原神/キャラクター"],
+    "living-being": ["living-being.csv", "名詞", "原神/生物"],
+    "enemy": ["enemy/enemy.csv", "名詞", "原神/敵"],
+    "boss": ["enemy/boss.csv", "名詞", "原神/ボス"],
 }
-facility_other = []
-area = {
-    "mondstadt": [],
-    "dragonspine": [],
-    "liyue": [],
-    "inazuma": [],
-    "sumeru": [],
-    "fontaine": [],
-    "natlan": [],
-    "snezhnaya": [],
-    "khaenriah": []
-}
-area_other = []
-element = []
-artifact_set = []
-artifact_piece = []
-character_main = []
-living_being = []
-enemy = []
-boss = []
+
+extracted = {}
+
+
+def find(q, tags):
+    result = []
+    for k, v in q.items():
+        if k in tags:
+            result.append(k)
+            if isinstance(v, list):
+                return result, v
+            elif isinstance(v, dict):
+                res = find(v, tags)
+                result.extend(res[0])
+                return result, res[1]
+
 
 for data in dataset:
     if "ja" not in data:
@@ -80,72 +96,35 @@ for data in dataset:
         continue
     if "tags" in data:
         tags = data["tags"]
-        if "weapon" in tags:
-            for weapon_type in weapon.keys():
-                if weapon_type in tags:
-                    weapon[weapon_type].append(data)
-                    break
+        res = find(q, tags)
+        if res:
+            k = "+".join(res[0])
+            if k in extracted:
+                extracted[k]["data"].append(data)
             else:
-                weapon_other.append(data)
-        elif "facility" in tags:
-            for region in facility.keys():
-                if region in tags:
-                    facility[region].append(data)
-                    break
-            else:
-                facility_other.append(data)
-        elif "location" in tags:
-            for region in area.keys():
-                if region in tags:
-                    area[region].append(data)
-                    break
-            else:
-                area_other.append(data)
-        elif "element" in tags:
-            element.append(data)
-        elif "artifact" in tags:
-            artifact_set.append(data)
-        elif "artifact-piece" in tags:
-            artifact_piece.append(data)
-        elif "character-main" in tags:
-            character_main.append(data)
-        elif "living-being" in tags:
-            living_being.append(data)
-        elif "enemy" in tags:
-            enemy.append(data)
-        elif "enemy-boss" in tags:
-            boss.append(data)
+                extracted[k] = {"data": [data], "args": res[1]}
 
-write(weapon["sword"], "weapon/sword.csv", "名詞", "原神/武器/片手剣")
-write(weapon["claymore"], "weapon/claymore.csv", "名詞", "原神/武器/両手剣")
-write(weapon["polearm"], "weapon/polearm.csv", "名詞", "原神/武器/長柄武器")
-write(weapon["bow"], "weapon/bow.csv", "名詞", "原神/武器/弓")
-write(weapon["catalyst"], "weapon/catalyst.csv", "名詞", "原神/武器/法器")
-write(weapon_other, "weapon/other.csv", "名詞")
-write(facility["mondstadt"], "facility/mondstadt.csv", "名詞", "原神/施設/モンド")
-write(facility["dragonspine"], "facility/dragonspine.csv", "名詞", "原神/施設/ドラゴンスパイン")
-write(facility["liyue"], "facility/liyue.csv", "名詞", "原神/施設/璃月")
-write(facility["inazuma"], "facility/inazuma.csv", "名詞", "原神/施設/稲妻")
-write(facility["sumeru"], "facility/sumeru.csv", "名詞", "原神/施設/スメール")
-write(facility["fontaine"], "facility/fontaine.csv", "名詞", "原神/施設/フォンテーヌ")
-write(facility["natlan"], "facility/natlan.csv", "名詞", "原神/施設/ナタ")
-write(facility["snezhnaya"], "facility/snezhnaya.csv", "名詞", "原神/施設/スネージナヤ")
-write(facility["khaenriah"], "facility/khaenriah.csv", "名詞", "原神/施設/カーンルイア")
-write(facility_other, "facility/other.csv", "名詞")
-write(area["mondstadt"], "area/mondstadt.csv", "地名その他", "原神/地名/モンド")
-write(area["dragonspine"], "area/dragonspine.csv", "地名その他", "原神/地名/ドラゴンスパイン")
-write(area["liyue"], "area/liyue.csv", "地名その他", "原神/地名/璃月")
-write(area["inazuma"], "area/inazuma.csv", "地名その他", "原神/地名/稲妻")
-write(area["sumeru"], "area/sumeru.csv", "地名その他", "原神/地名/スメール")
-write(area["fontaine"], "area/fontaine.csv", "地名その他", "原神/地名/フォンテーヌ")
-write(area["natlan"], "area/natlan.csv", "地名その他", "原神/地名/ナタ")
-write(area["snezhnaya"], "area/snezhnaya.csv", "地名その他", "原神/地名/スネージナヤ")
-write(area["khaenriah"], "area/khaenriah.csv", "地名その他", "原神/地名/カーンルイア")
-write(area_other, "area/other.csv", "地名その他")
-write(element, "element.csv", "名詞")
-write(artifact_set, "artifact/set.csv", "名詞", "原神/聖遺物")
-write(artifact_piece, "artifact/piece.csv", "名詞")
-write(character_main, "character/playable.csv", "人名", "原神/キャラクター")
-write(living_being, "living-being.csv", "名詞", "原神/生物")
-write(enemy, "enemy/enemy.csv", "名詞", "原神/敵")
-write(boss, "enemy/boss.csv", "名詞", "原神/ボス")
+
+def write(data_list, filepath, hinshi, comment=""):
+    if data_list:
+        with open(os.path.join(data_dir, filepath), "a", encoding="utf-8") as f:
+            print(f"新規追加 ({filepath}): ", len(data_list))
+            writer = csv.writer(f)
+            for data in data_list:
+                word = data["ja"]
+                hiragana = (
+                    jaconv.kata2hira(data["pronunciationJa"])
+                    if "pronunciationJa" in data
+                    else ""
+                )
+                _comment = ""
+                if comment:
+                    _comment = comment
+                elif "notes" in data:
+                    _comment = data["notes"]
+
+                writer.writerow([word, hiragana, hinshi, _comment])
+
+
+for v in extracted:
+    write(v["data"], *v["args"])
